14de3f72c9e530ed8131e515f60013c786469b09
Hashan000-1 <pabasarahashan977@gmail.com>
2025-05-03 20:17:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,17 +5,17 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "DnhwCARa#4l6RRqaPriyJRd6qEK-avG4Y-s8Xu1jeOrnW87KY2EI", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "FOE2QbAS#5RY1kjPXJdKIuy_fPTcF_ykUmz5sSrzbk3NA7VySmXE", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
-STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "*_`AUTO STATUS JUST NOW SEEN BY LARA MD`_*",
-MODE: process.env.MODE || "public",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "*_`AUTO STATUS JUST NOW SEEN BY LUNA MD`_*",
+MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",    
-AUTO_REACT: process.env.AUTO_REACT || "false",
+AUTO_REACT: process.env.AUTO_REACT || "true",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 ANTI_LINK: process.env.ANTI_LINK || "true",
